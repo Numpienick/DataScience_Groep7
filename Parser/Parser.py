d751ab38ed88af94541f78a2d4a9ba9edd3acaa7
Wim <w.j.stienstra@home.nl>
2022-03-15 15:38:42 +0100

    Working Rating csv converter

--- Parser/Parser.py
+++ Parser/Parser.py
@@ -3,25 +3,35 @@ import csv
 
 from Regex import *
 
+
 def readFile(dataType):
     file = open("data/" + dataType.file + ".list", "r", encoding="ANSI")
     txt = file.read()
-    data = re.split(dataType.initialRegex, str(txt))
+    data = re.findall(dataType.initialRegex, str(txt))
     print("done reading")
     file.close()
     return data
 
+
 def writeCSV(data, name):
-    with open("output/" + name + '.csv', 'w') as f:
-        # create the csv writer
-        writer = csv.writer(f)
-        writer.writerow(data)
+    # create the csv writer
+    with open("output/" + name + '.csv', 'w', newline='') as f:
+        writer = csv.writer(f, delimiter=';', dialect="excel")
+        writer.writerows(data)
     print("done writing to CSV")
 
-#Movies
-movie = Movie()
-writeCSV(readFile(movie), movie.file)
 
-#Country
+# Movies
+movie = Movie()
+# writeCSV(readFile(movie), movie.file)
+
+# Country
 country = Country()
-writeCSV(readFile(country), country.file)
+# writeCSV(readFile(country), country.file)
+
+# Plot
+plot = Plot()
+# writeCSV(readFile(plot), plot.file)
+
+rating = Rating()
+writeCSV(readFile(rating), rating.file)
--- Parser/Regex.py
+++ Parser/Regex.py
@@ -1,11 +1,22 @@
 class Movie:
     initialRegex = "(?s).+?(?=(\"))(.+?(?=(--------------------------------------------------------------------------------)))"
+    regex = ""
     file = "movies"
+
 
 class Actor:
     regex = ""
     file = "actors"
 
+
 class Country:
-    initialRegex = "^(.*)==============([\s\S]*)*"
+    initialRegex = "[^==============]*$"
     file = "countries"
+
+class Plot:
+    initialRegex = "MV[\s\S]*?-------------------------------------------------------------------------------"
+    file = "plot"
+
+class Rating:
+    initialRegex = "\s*?(?P<Distribution>[0-9\.*]{10})\s+?(?P<Votes>\d+)\s+?(?P<Rating>.\d\.\d)\s+?(?P<Title>.*?)\((?P<Year>\d{4}).*?\)|\s+?\((?P<Platform>(?:TV)|(?:V)|(?:VG))\)\s*?|{(?P<Episode>.*?)}"
+    file = "ratings"
