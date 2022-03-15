ccd38aa90f243076e0fe669fbb6af4b96c4c4b67
Wim <w.j.stienstra@home.nl>
2022-03-15 14:09:35 +0100

    Moved reading and writing to function, added regex of country

--- Parser/Parser.py
+++ Parser/Parser.py
@@ -1,31 +1,27 @@
 import re
 import csv
 
-# actorsList = open("data/movies.list", "r", encoding="ANSI")
-# print(actorsList.read())
-
-#Movies
 from Regex import *
 
+def readFile(dataType):
+    file = open("data/" + dataType.file + ".list", "r", encoding="ANSI")
+    txt = file.read()
+    data = re.split(dataType.initialRegex, str(txt))
+    print("done reading")
+    file.close()
+    return data
+
+def writeCSV(data, name):
+    with open("output/" + name + '.csv', 'w') as f:
+        # create the csv writer
+        writer = csv.writer(f)
+        writer.writerow(data)
+    print("done writing to CSV")
+
+#Movies
 movie = Movie()
-file = open("data/"+movie.file+".list", "r", encoding="ANSI")
-txt = file.read()
-movies = re.split(movie.initialRegex, str(txt))
+writeCSV(readFile(movie), movie.file)
 
-#opens file, writes every element of array with \n between
-# with open("text.txt", 'w') as file:
-#     file.write('\n'.join(x))
-
-with open('text.csv', 'w') as f:
-    # create the csv writer
-    writer = csv.writer(f)
-    writer.writerow(movies)
-    # for i in x:
-    #     # write a row to the csv file
-    #     writer.writerow(i)
-    #     print(i)
-print("done")
-
-file.close()
-
-#Actors
+#Country
+country = Country()
+writeCSV(readFile(country), country.file)
--- Parser/Regex.py
+++ Parser/Regex.py
@@ -1,9 +1,11 @@
-#Movies
 class Movie:
     initialRegex = "(?s).+?(?=(\"))(.+?(?=(--------------------------------------------------------------------------------)))"
-
     file = "movies"
 
 class Actor:
     regex = ""
     file = "actors"
+
+class Country:
+    initialRegex = "^(.*)==============([\s\S]*)*"
+    file = "countries"
