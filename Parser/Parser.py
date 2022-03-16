7810bb1468782f6882cc16138e0eb6dc129c6e11
Wim <w.j.stienstra@home.nl>
2022-03-16 12:03:53 +0100

    Changed readfile a bit to keep it "enclosed". Added actor, actress, cinematographer, director

--- Parser/Parser.py
+++ Parser/Parser.py
@@ -5,12 +5,11 @@ from Regex import *
 
 
 def readFile(dataType):
-    file = open("data/" + dataType.file + ".list", "r", encoding="ANSI")
-    txt = file.read()
-    data = re.findall(dataType.initialRegex, str(txt))
-    print("done reading")
-    file.close()
-    return data
+    with open("data/" + dataType.file + ".list", "r", encoding="ANSI") as f:
+        txt = f.read()
+        data = re.findall(dataType.regex, str(txt))
+        print("done reading " + dataType.file)
+        return data
 
 
 def writeCSV(data, name):
@@ -18,12 +17,12 @@ def writeCSV(data, name):
     with open("output/" + name + '.csv', 'w', encoding="ANSI", newline='') as f:
         writer = csv.writer(f, delimiter=';', dialect="excel")
         writer.writerows(data)
-    print("done writing to CSV")
+        print("done writing to " + name + ".csv")
 
 
 # Movies
 movie = Movie()
-writeCSV(readFile(movie), movie.file)
+# writeCSV(readFile(movie), movie.file)
 
 # Country
 country = Country()
@@ -34,4 +33,16 @@ plot = Plot()
 # writeCSV(readFile(plot), plot.file)
 
 rating = Rating()
-writeCSV(readFile(rating), rating.file)
+# writeCSV(readFile(rating), rating.file)
+
+actress = Actress()
+# writeCSV(readFile(actress), actress.file)
+
+actor = Actor()
+# writeCSV(readFile(actor), actor.file)
+
+cinematographer = Cinematographer()
+# writeCSV(readFile(cinematographer), cinematographer.file)
+
+director = Director()
+writeCSV(readFile(director), director.file)
