349853cc6b0aed202600436bcfe42c9145ad3739
Wim <w.j.stienstra@home.nl>
2022-03-16 13:31:19 +0100

    Added choice for choosing data to be parsed. Added working Country

--- Parser/Parser.py
+++ Parser/Parser.py
@@ -5,46 +5,64 @@ from Regex import *
 
 
 def readFile(dataType):
-    with open("data/" + dataType.file + ".list", "r", encoding="ANSI") as f:
-        txt = f.read()
-        data = re.findall(dataType.regex, str(txt))
-        print("done reading " + dataType.file)
-        return data
+    print("\nStarts reading " + dataType.file)
+    try:
+        with open("data/" + dataType.file + ".list", "r", encoding="ANSI") as f:
+            txt = f.read()
+            data = re.findall(dataType.regex, str(txt))
+            print("Done reading " + dataType.file)
+            return data
+    except IOError:
+        print("File could not be opened: " + dataType.file)
 
 
 def writeCSV(data, name):
-    # create the csv writer
-    with open("output/" + name + '.csv', 'w', encoding="ANSI", newline='') as f:
-        writer = csv.writer(f, delimiter=';', dialect="excel")
-        writer.writerows(data)
-        print("done writing to " + name + ".csv")
+    print("\nStarts writing " + name)
+    try:
+        with open("output/" + name + '.csv', 'w', encoding="ANSI", newline='') as f:
+            writer = csv.writer(f, delimiter=';', dialect="excel")
+            writer.writerows(data)
+            print("Done writing to " + name + ".csv")
+    except IOError:
+        print("File could not be created: " + name + ".csv")
 
 
-# Movies
-movie = Movie()
-# writeCSV(readFile(movie), movie.file)
+print("Welkom bij de IMDB-Parser van groep 7")
+print("Welke dataset wil je omzetten naar CSV?")
+print(
+    "1. Actors\n2. Actresses \n3. Cinematographers \n4. Countries \n5. Directors \n6. Genres \n7. Movie \n8. Plot \n9. Ratings \n10. Running Times \n0. Allemaal")
 
-# Country
-country = Country()
-# writeCSV(readFile(country), country.file)
+dataSetChoice = int(input())
 
-# Plot
-plot = Plot()
-# writeCSV(readFile(plot), plot.file)
-
-rating = Rating()
-# writeCSV(readFile(rating), rating.file)
-
-actress = Actress()
-# writeCSV(readFile(actress), actress.file)
-
-actor = Actor()
-# writeCSV(readFile(actor), actor.file)
-
-cinematographer = Cinematographer()
-# writeCSV(readFile(cinematographer), cinematographer.file)
-
-director = Director()
-writeCSV(readFile(director), director.file)
+if dataSetChoice == 1 or dataSetChoice == 0:
+    actor = Actor()
+    writeCSV(readFile(actor), actor.file)
+if dataSetChoice == 2 or dataSetChoice == 0:
+    actress = Actress()
+    writeCSV(readFile(actress), actress.file)
+if dataSetChoice == 3 or dataSetChoice == 0:
+    cinematographer = Cinematographer()
+    writeCSV(readFile(cinematographer), cinematographer.file)
+if dataSetChoice == 4 or dataSetChoice == 0:
+    country = Country()
+    writeCSV(readFile(country), country.file)
+if dataSetChoice == 5 or dataSetChoice == 0:
+    director = Director()
+    writeCSV(readFile(director), director.file)
+if dataSetChoice == 6 or dataSetChoice == 0:
+    genre = Genre()
+    writeCSV(readFile(genre), genre.file)
+if dataSetChoice == 7 or dataSetChoice == 0:
+    movie = Movie()
+    writeCSV(readFile(movie), movie.file)
+if dataSetChoice == 8 or dataSetChoice == 0:
+    plot = Plot()
+    writeCSV(readFile(plot), plot.file)
+if dataSetChoice == 9 or dataSetChoice == 0:
+    rating = Rating()
+    writeCSV(readFile(rating), rating.file)
+if dataSetChoice == 10 or dataSetChoice == 0:
+    runningTime = RunningTime()
+    writeCSV(readFile(runningTime), runningTime.file)
 
 print("Done!")
--- Parser/Regex.py
+++ Parser/Regex.py
@@ -34,8 +34,9 @@ class Cinematographer(DataSet):
 class Country(DataSet):
     def __init__(self):
         super().__init__()
-        self.regex = "[^==============]*$"
+        self.regex = "(?:\")?(?P<ShowTitle>.+?)(?:\")?\s\((?P<ReleaseDateOrder>\d{4}(?:[^)]+?)?|\?{4}(?:.+?)?)\)\s+?(?:\((?P<ShowType>(?:TV)|(?:V)|(?:VG))\))?(?:\{(?P<EpisodeTitle>(?:(?!\(\#|\{).+?(?= \()|(?!\(\#|\{).+?(?=\}))?))?(?:\})?(?:\s)?(?:\(\#(?P<SeasonNumber>\d+?)\.(?P<EpisodeNumber>\d+?)\)\})?(?:\s)?(?:(?P<Suspended>\{\{SUSPENDED\}\}))?(?:\0+?)?(?:\s+?)?(?P<CountriesOfOrigin>\w.+)"
         self.file = "countries"
+
 
 class Director(DataSet):
     def __init__(self):
@@ -43,11 +44,27 @@ class Director(DataSet):
         self.regex = "((?:(?P<LastName>.+?)\,\s)?(?P<FirstName>.+?)|\t\t\t)\t(\"(?P<SeriesTitle>(?!\t).+?)\"|(?P<Title>(?!\t).+?))\s\((?P<ReleaseDate>.+?)\)(?:\s)?(?:\{((?P<EpisodeName>(?!\{).+?)(?:\s\(\#(?P<Season>[0-9]{1,})\.(?P<Episode>[0-9]{1,})\))?)\}\s)?(?:\((?P<ShowType>[TVG]{1,2})\))?(?:.+?\((?P<Voicing>voice)\))?(?:.+?\((?P<Archive>archive footage)\))?(?:.+?\((?P<PlayedAs>as\s.*)\))?(?:.+?\((?P<Uncredited>uncredited)\))?(?:\{\{(?P<Suspended>SUSPENDED)\}\})?(?:.+?\[((?P<Role>.+?)(?:\(segment\s\"(?P<Segment>.+?)\"\))?)\])?(?:.+?\<(?P<Position>[0-9]{1,})\>)?"
         self.file = "directors"
 
+
+class Genre(DataSet):
+    def __init__(self):
+        super().__init__()
+        self.regex = ""
+        self.file = "genres"
+
+
 class Movie(DataSet):
     def __init__(self):
         super().__init__()
         self.regex = "(?:\")?(?P<ShowTitle>.+?)(?:\")?\s\((?P<ReleaseDateOrder>\d{4}(?:[^)]+?)?|\?{4}(?:.+?)?)\)\s+?(?:\((?P<ShowType>(?:TV)|(?:V)|(?:VG))\))?(?:\{(?P<EpisodeTitle>(?:(?!\(\#|\{).+?(?= \()|(?!\(\#|\{).+?(?=\}))?))?(?:\})?(?:\s)?(?:\(\#(?P<SeasonNumber>\d+?)\.(?P<EpisodeNumber>\d+?)\)\})?(?:\s)?(?:(?P<Suspended>\{\{SUSPENDED\}\}))?(?:\0+?)?(?:\s+?)?(?P<ReleaseYear>\d{4}|\?{4})(?:-(?P<EndYear>\d{4}|\?{4}))?"
         self.file = "movies"
+
+
+class Plot(DataSet):
+    def __init__(self):
+        super().__init__()
+        self.regex = "(MV[\s\S]*?-------------------------------------------------------------------------------)"
+        self.file = "plot"
+
 
 class Rating(DataSet):
     def __init__(self):
@@ -56,8 +73,8 @@ class Rating(DataSet):
         self.file = "ratings"
 
 
-class Plot(DataSet):
+class RunningTime(DataSet):
     def __init__(self):
         super().__init__()
-        self.regex = "(MV[\s\S]*?-------------------------------------------------------------------------------)"
-        self.file = "plot"
+        self.regex = ""
+        self.file = "running-times"
